f60ed5f8553a8388e8c0e313945d63e3cf15d804
Jacobs <kudyakov687@mail.ru>
2024-03-14 13:03:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleAppService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Отправка запроса к веб-сервису
+        using var client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync("http://localhost:5000/WeatherForecast");
+
+        // Обработка ответа
+        if (response.IsSuccessStatusCode)
+        {
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseContent);
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: {response.StatusCode}");
+        }
+
+        // Запись в лог-файл
+        string logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "log.txt");
+        string logMessage = "Запись в лог-файл: " + DateTime.Now.ToString();
+        File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+    }
+}
